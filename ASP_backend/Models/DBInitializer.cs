cd1352c4fcbb1ea3a251793c3627a9619186aa25
Maarten Michiels <mnmmaarten@hotmail.com>
2020-01-23 14:38:22 +0100

    fixed inintial data load :D

--- ASP_backend/Models/DBInitializer.cs
+++ ASP_backend/Models/DBInitializer.cs
@@ -25,18 +25,25 @@ namespace ASP_backend.Models
                 new Plaats { Naam = "Rol brug", Xcord = 0.5555, Ycord = 0.5555 }
                 );
 
-            //context.Personen.AddRange(
-            //    new Persoon { Naam = "Michiels", Usernaam = "mnmpower", Voornaam = "Maarten", TypeID = 1, Wachtwoord = "R1234-56" },
-            //    new Persoon { Naam = "Admin", Usernaam = "Admin", Voornaam = "Admin", TypeID = 1, Wachtwoord = "Admin" },
-            //    new Persoon { Naam = "Gebruiker", Usernaam = "Gebruiker", Voornaam = "Gebruiker", TypeID = 2, Wachtwoord = "Gebruiker" },
-            //    new Persoon { Naam = "g", Usernaam = "g", Voornaam = "g", TypeID = 2, Wachtwoord = "g" },
-            //    new Persoon { Naam = "a", Usernaam = "a", Voornaam = "a", TypeID = 1, Wachtwoord = "g" }
-            //    );
 
-            //context.Meldingen.AddRange(
-            //    new Melding { PersoonID = 3, PlaatsID = 1, Tijdstip = DateTime.Now },
-            //    new Melding { PersoonID = 4, PlaatsID = 2, Tijdstip = DateTime.Now }
-            //    );
+            context.SaveChanges();
+
+            //context.Personen.Add(new Persoon { Naam = "Michiels", Usernaam = "mnmpower", Voornaam = "Maarten", TypeID = 1, Wachtwoord = "R1234-56" });
+
+            context.Personen.AddRange(
+                new Persoon { Naam = "Michiels", Usernaam = "mnmpower", Voornaam = "Maarten", TypeID = 1, Wachtwoord = "R1234-56" },
+                new Persoon { Naam = "Admin", Usernaam = "Admin", Voornaam = "Admin", TypeID = 1, Wachtwoord = "Admin" },
+                new Persoon { Naam = "Gebruiker", Usernaam = "Gebruiker", Voornaam = "Gebruiker", TypeID = 2, Wachtwoord = "Gebruiker" },
+                new Persoon { Naam = "g", Usernaam = "g", Voornaam = "g", TypeID = 2, Wachtwoord = "g" },
+                new Persoon { Naam = "a", Usernaam = "a", Voornaam = "a", TypeID = 1, Wachtwoord = "g" }
+                );
+
+            context.SaveChanges();
+
+            context.Meldingen.AddRange(
+                new Melding { PersoonID = 3, PlaatsID = 1, Tijdstip = DateTime.Now },
+                new Melding { PersoonID = 4, PlaatsID = 2, Tijdstip = DateTime.Now }
+                );
 
 
             context.SaveChanges();
